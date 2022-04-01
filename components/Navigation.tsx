42babd708f999b42a5eca714cfb9c9182febe21c
Ethan <ezoller11@gmail.com>
2022-04-01 10:07:56 -0700

    Added collections page

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -11,6 +11,9 @@ const Navigation = () => {
           <Logo />
         </a>
       </Link>
+      <Link href='/'>
+        <a>Collections</a>
+      </Link>
     </StyledNavigation>
   )
 }
@@ -21,4 +24,23 @@ const StyledNavigation = styled.nav`
   background: var(--color-alabaster);
   border: 1px solid var(--color-mercury);
   padding: 16px 24px;
+  display: flex;
+  align-items: center;
+
+  a {
+    transition: 0.2s ease;
+
+    :first-child {
+      margin-right: 16px;
+    }
+
+    :last-child {
+      font-size: 14px;
+      color: var(--color-dove);
+
+      :hover {
+        color: var(--color-black);
+      }
+    }
+  }
 `
